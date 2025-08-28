55b8ed0e038aaa02a7422833bc21490d89ab8f5e
Tran Nghia <tplnghia2416@apcs.fitus.edu.vn>
2025-08-28 08:46:55 +0700

    A

--- src/Level.cpp
+++ src/Level.cpp
@@ -161,7 +161,7 @@ void Level_1_1::render()
             if (tileInstancesGrid[i][j])
             {
                 tileInstancesGrid[i][j]->render();
-                // DrawRectangleLinesEx(tileInstancesGrid[i][j]->bbox, 2.0f, RED); // Debug: Draw bounding box
+                // ÂRectangleLinesEx(tileInstancesGrid[i][j]->bbox, 2.0f, RED); // Debug: Draw bounding box
             }
         }
     }
@@ -295,7 +295,7 @@ void Level_1_3::render()
             if (tileInstancesGrid[i][j])
             {
                 tileInstancesGrid[i][j]->render();
-                DrawRectangleLinesEx(tileInstancesGrid[i][j]->bbox, 2.0f, RED); // Draw bounding box
+                // DrawRectangleLinesEx(tileInstancesGrid[i][j]->bbox, 2.0f, RED); // Draw bounding box
             }
         }
     }
